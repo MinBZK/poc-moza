5ddef7c7efb5f67abf54dae2f5599002a27affdc
Ravi Meijer <ravi.meijer@rijksoverheid.nl>
2026-03-23 11:40:51 +0100

    fix: token prefix from --mox to --toepassing)

--- _site/storybook/assets/formatter-EIJCOSYU-K51lXwTS.js
+++
@@ -1,1 +0,0 @@
-import{d as m,a as e}from"./DocsRenderer-LL677BLK-DvMYlYPO.js";import{b as o}from"./iframe-BFHU87YF.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
