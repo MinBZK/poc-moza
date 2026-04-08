354cb1e2b6b4ea6af89fbb34a2d3ce0da7aa9f60
Ravi Meijer <ravi.meijer@rijksoverheid.nl>
2026-04-08 09:17:39 +0200

    fix: update styling

--- _site/storybook/assets/formatter-EIJCOSYU-CcUV935c.js
+++
@@ -1,1 +0,0 @@
-import{d as m,a as e}from"./DocsRenderer-LL677BLK-CjuY0Mcq.js";import{b as o}from"./iframe-B4lkCQat.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
