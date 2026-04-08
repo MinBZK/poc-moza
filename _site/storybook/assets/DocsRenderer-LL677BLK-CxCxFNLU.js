dda655cf2a970aadd8dc2dd2572edb3c8e211190
Rogier Barendregt <rogier.barendregt@rijksoverheid.nl>
2026-04-08 17:58:34 +0200

    🧼 Storybook rebuild

--- _site/storybook/assets/DocsRenderer-LL677BLK-CxCxFNLU.js
+++ _site/storybook/assets/DocsRenderer-LL677BLK-CxCxFNLU.js
@@ -1,4 +1,4 @@
-import{k as Qh,m as Gh,n as Ch,r as Gn,R as ye,H as Rh,o as Zh,C as jh,D as ph,u as Vh}from"./blocks-CU519ART.js";import{_ as xh}from"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DLl1_a5I.js";const Kh=Object.freeze(Object.defineProperty({__proto__:null,MDXProvider:Qh,useMDXComponents:Vh},Symbol.toStringTag,{value:"Module"}));var ui={exports:{}},ve={},ei={exports:{}},ni={};/**
+import{k as Qh,m as Gh,n as Ch,r as Gn,R as ye,H as Rh,o as Zh,C as jh,D as ph,u as Vh}from"./blocks-9eKEkKWO.js";import{_ as xh}from"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Cgm1CB6M.js";const Kh=Object.freeze(Object.defineProperty({__proto__:null,MDXProvider:Qh,useMDXComponents:Vh},Symbol.toStringTag,{value:"Module"}));var ui={exports:{}},ve={},ei={exports:{}},ni={};/**
  * @license React
  * scheduler.production.js
  *
--- _site/storybook/assets/Kleurenpalet-B6IxzvtP.js
+++
@@ -1,3 +0,0 @@
-import{u as r,j as t,M as a,S as n}from"./blocks-CU519ART.js";import{K as i,R as m,T as p}from"./Kleurenpalet.stories-DhvIr_zM.js";import"./preload-helper-Dp1pzeXC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DLl1_a5I.js";function e(s){return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:i}),`
-`,t.jsx(n,{of:m}),`
-`,t.jsx(n,{of:p})]})}function l(s={}){const{wrapper:o}={...r(),...s.components};return o?t.jsx(o,{...s,children:t.jsx(e,{...s})}):e()}export{l as default};
